8ab0819957c3bb621eddacc2bc38dbe7cf8e3386
NBerkel <NBerkel@users.noreply.github.com>
2017-05-27 10:23:33 +1000

    Switched user system to use Google Accounts

--- client/partials/Register.js
+++ client/partials/Register.js
@@ -11,22 +11,3 @@ Template.Register.events({
       });
     }
 });
-
-Template.Register.events({
-    'submit form': function(event) {
-         event.preventDefault();
-
-         var registerData = {
-            email: event.target.registerEmail.value,
-            password: event.target.registerPassword.value
-         }
-
-         Accounts.createUser(registerData, function(error){
-            if (Meteor.user()) {
-               console.log(Meteor.userId());
-            } else {
-                document.getElementById('form-feedback').innerHTML = error.reason;
-            }
-         });
-     }
-})
